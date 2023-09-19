ec77d076d576867b735f22b05b6b17a46dfac269
jordanmefujie <mjordanlonpo@gmail.com>
2023-09-19 08:39:57 -0700

    Commit for test folder

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -15,9 +15,9 @@ class TestSquare(unittest.TestCase):
 
         s2 = Square(10)
 
-        self.assertEqual(s1.id, 5)
+        self.assertEqual(s1.id, 13)
 
-        self.assertEqual(s2.id, 6)
+        self.assertEqual(s2.id, 14)
 
 
     def test_initialization_without_arguments(self):
