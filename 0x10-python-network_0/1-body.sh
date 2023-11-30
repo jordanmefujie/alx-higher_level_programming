a08f4ee3e1f28d90d9268564d6c4d8e077741230
jordanmefujie <mjordanlonpo@gmail.com>
2023-11-30 13:02:56 -0800

    Tasks completed

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 # Check if URL is provided as an argument
-curl -s -w "%{http_code}" "$1" | awk 'END {if ($0 == 200) print ""; else print $0}'
+curl -sX GET $1 -L
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods
-curl -sI -X OPTIONS "$1" | grep "Allow" | awk '{print $2}'
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
