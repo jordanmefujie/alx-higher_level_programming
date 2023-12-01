a8a0163ecb6c32d1dc94dd920887988c849d74d0
jordanmefujie <mjordanlonpo@gmail.com>
2023-12-01 14:17:57 -0800

    All corrections made.

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """
-takes your Github credentials (username and password) and uses the
-Github API to display your id
+takes your GitHub credentials (username and personal access token) and
+uses the GitHub API to display your id
 """
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
     """
-    takes your Github credentials (username and password) and uses the
-    Github API to display your id
+    takes your GitHub credentials (username and personal access token) and
+    uses the GitHub API to display your id
     """
     username = argv[1]
-    password = argv[2]
+    token = argv[2]
     url = 'https://api.github.com/user'
-    r = requests.get(url, auth=(username, password))
+    headers = {'Authorization': f'Basic {jordanmefujie}:{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}'} # noqa
+    
     try:
-        print(r.json().get('id'))
-    except json.JSONDecodeError:
-        pass
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        user_id = response.json().get('id')
+        print(f"Your GitHub user id: {user_id}")
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+    except ValueError:
+        print("Not a valid JSON response")
